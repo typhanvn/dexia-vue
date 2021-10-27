db72ffe7d1c22b2ae7f385b60e3e21f7651c2ef2
Thanh Ty <thanhty0616@gmail.com>
2021-10-27 11:59:19 +0700

    Update source

---
+++ layouts/empty.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nuxt></nuxt>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'Empty',
+  setup() {},
+})
+</script>
