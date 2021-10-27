912ca0d55b55fe62e3140ac2fce5cefc155bfdf3
Ty Phan <49025355+typhanvn@users.noreply.github.com>
2021-10-27 11:27:05 +0700

    Init source

---
+++ _nuxt/static/1635308534/fr/user/220/edit/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635308534",layout:"empty",error:{statusCode:404,path:a,message:"This page could not be found"},state:{agenda:{}},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null}},globalRefs:{}}}("\u002Ffr\u002Fuser\u002F220\u002Fedit"));
